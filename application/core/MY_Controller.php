37b1445a459495d0164db67d2a0bfb6d1922eaff
Ijul Farizul <farxpeace@gmail.com>
2021-01-30 08:20:03 +0800

    sdfsdfsdf

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -8,6 +8,7 @@ class MY_Controller extends CI_Controller {
     
     public $Player;
     function __construct() {
+        $this->onload_check_modules();
         parent::__construct();
         
         
@@ -20,6 +21,18 @@ class MY_Controller extends CI_Controller {
         $this->data = array();
         
         
+    }
+    
+    function onload_check_modules(){
+        $list_not_found = array();
+        $required = array("sqlite3", "pdo_sqlite");
+        $installed_extension = get_loaded_extensions();
+        foreach($installed_extension as $a => $b){
+            if(!in_array($b, $required)){
+                $list_not_found[] = $b;
+            }
+        }
+        echo "<pre>"; print_r($list_not_found); exit();
     }
     
     function detect_player_available(){
